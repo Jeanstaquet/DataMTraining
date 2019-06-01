aa51265ac26fe40c329bed471bd844dbe5725f1f
Jean Staquet <jean.staquet8@gmail.com>
2019-06-01 23:03:12 +0200

    ajout de plot, version du dataset sans les na, case_when qui fonctionne, a faire = tout et voir sur youtube ce que les autres ont fait

--- Tidytuesday_Wine_2019-05-28.R
+++ Tidytuesday_Wine_2019-05-28.R
@@ -8,7 +8,48 @@ countryvect <- country1[[1, ]]
 wine_ratings1 <- wine_ratings %>% group_by(country) %>% filter(country %in% countryvect) %>% summarise(avg_point = mean(points))
 wine_ratings2 <- na.omit(wine_ratings1)
 
+## country by there average score point
 wine_ratings2 %>% ggplot(aes(reorder(country, avg_point), avg_point)) + 
       geom_point() +
-      coord_flip()
-wine_ratings %>% group_by(country) %>% 
+      coord_flip() + 
+      geom_hline(yintercept = c(86, 88, 90)) +
+      scale_y_continuous("Mean of points", expand = c(0,0.3)) + 
+      scale_x_discrete("Country") + 
+      labs(title = "Average score per country") + 
+      theme_economist()
+
+## country by points boxplot
+wine_ratings %>% group_by(country) %>% filter(country %in% countryvect) %>% sample_n(size = 30) %>% ggplot(aes(reorder(country, points), points)) +
+      geom_boxplot(alpha = 0.2) +
+      geom_point(col = "blue", position = "jitter", alpha = 0.3) +
+      theme(axis.text.x = element_text(angle = 90))
+
+##country by points errorbar
+wine_ratings %>% group_by(country) %>% filter(country %in% countryvect) %>% sample_n(size = 30) %>% ggplot(aes(reorder(country, points), points)) +
+      stat_summary(fun.data = mean_sdl, fun.args = list(mult = 1), geom = "errorbar") +
+      stat_summary(fun.y = mean, geom = "point") +
+      theme_economist() +
+      theme(axis.text.x = element_text(angle = 90))
+
+## price by points + case_when
+max(wine_ratings$price, na.rm = TRUE)
+min(wine_ratings$price, na.rm = TRUE)
+
+wine_price <- wine_ratings %>% filter(!is.na(price)) %>% mutate(economic_rate = case_when(price <= 25 ~ "normal",
+                                                                            price <= 100 ~ "high", 
+                                                                            price <= 1000 ~ "very high",
+                                                                            price <= 3300 ~ "no limit"))
+wine_price_omit <- na.omit(wine_price)
+
+wine_price %>% ggplot(aes(points, price)) +
+      geom_point() +
+      geom_smooth(method = "lm", se = FALSE)
+
+
+top_producer <- wine_price %>% count(province) %>% arrange(desc(n)) %>% head(20) %>% select(-2)
+province_top_p <- top_producer[[1, ]]                                                  
+wine_ratings %>% filter(province %in% province_top_p) %>% 
+      ggplot(aes(province, price)) +
+      geom_boxplot() + 
+      scale_y_log10() + 
+      theme(axis.text.x = element_text(angle = 90))
